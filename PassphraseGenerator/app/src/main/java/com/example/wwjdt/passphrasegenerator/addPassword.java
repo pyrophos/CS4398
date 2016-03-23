8d80f17d5338619128ccbaa3122d6da1f967804b
dylanjphelps <dylanjphelps@yahoo.com>
2016-03-22 19:04:30 -0500

    Added password creation activity

---
+++ PassphraseGenerator/app/src/main/java/com/example/wwjdt/passphrasegenerator/addPassword.java
@@ -0,0 +1,13 @@
+package com.example.wwjdt.passphrasegenerator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_password);
+    }
+}
