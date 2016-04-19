45496dba5c4f9338b2b410f1340b6158ef1766a2
pyrophos <adamponcy@gmail.com>
2016-04-19 17:03:03 -0500

    Adding initial unit tests for Credential generator

--- PassphraseGenerator/app/src/test/java/com/example/wwjdt/passphrasegenerator/CredentialTest.java
+++ PassphraseGenerator/app/src/test/java/com/example/wwjdt/passphrasegenerator/CredentialTest.java
@@ -1,17 +1,13 @@
 package com.example.wwjdt.passphrasegenerator;
 
 import org.junit.Test;
+import java.util.regex.Pattern;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import static org.junit.Assert.*;
-
-/**
- * To work on unit tests, switch the Test Artifact in the Build Variants view.
- */
 public class CredentialTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
 
-
+  @Test
+  public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+  }
 }
