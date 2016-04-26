041d6688a7da4438bef6253d5f4a0810e7231fa9
pyrophos <adamponcy@gmail.com>
2016-04-25 19:25:36 -0500

    Updated unit tests.

--- PassphraseGenerator/Passphrase Generator/src/test/java/com/example/wwjdt/passphrasegenerator/CredentialTest.java
+++ PassphraseGenerator/Passphrase Generator/src/test/java/com/example/wwjdt/passphrasegenerator/CredentialTest.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
-public class CredentialTest{
+public class CredentialTest {
 
   WordModel testWordModel = new WordModel();
 
